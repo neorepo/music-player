4663678c9ab5e7742f8938cb07fbf0eff879e10a
neorepo <neorepo.edu@gmail.com>
2025-09-02 23:07:12 -0300

    Updating files

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@
 const songs = [
     { id: 1, artist: "Eddie Santiago", title: "Antidoto y veneno", src: "audio/salsa/Eddie Santiago - Antidoto y veneno.m4a", duration: "5:18" },
     { id: 2, artist: "La inmensidad", title: "Ay que amor", src: "audio/salsa/La inmensidad - Ay que amor.mp3", duration: "3:54" },
-    { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: "5:43" },
+    { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: "5:44" },
     { id: 4, artist: "Los Hermanos Moreno", title: "Por alguien como tu", src: "audio/salsa/Los Hermanos Moreno - Por alguien como tu.m4a", duration: "3:38" },
     { id: 5, artist: "Nino Segarra", title: "Como amigos si, como amantes no", src: "audio/salsa/Nino Segarra - Como amigos si, como amantes no.mp3", duration: "4:46" },
     { id: 6, artist: "Nino Segarra", title: "Entre la espada y la pared", src: "audio/salsa/Nino Segarra - Entre la espada y la pared.mp3", duration: "5:44" },
-    { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: "5:06" },
-    { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: "5:32" },
+    { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: "5:07" },
+    { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: "5:33" },
     { id: 9, artist: "Pedro Arroyo", title: "Todo me huele a ti", src: "audio/salsa/Pedro Arroyo - Todo me huele a ti.mp3", duration: "5:22" },
     { id: 10, artist: "The New York Band", title: "Nadie como tu", src: "audio/salsa/The New York Band - Nadie como tu.m4a", duration: "4:47" },
     { id: 11, artist: "Willie Gonzalez", title: "Hazme olvidarla", src: "audio/salsa/Willie Gonzalez - Hazme olvidarla.mp3", duration: "5:36" },
-    { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: "5:32" },
-    { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: "5:07" },
-    { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: "5:03" },
-    { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: "5:08" },
+    { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: "5:33" },
+    { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: "5:08" },
+    { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: "5:04" },
+    { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: "5:09" },
 ];
 
 // Ordenamos el listado de canciones por artista
@@ -86,8 +86,7 @@ shuffleBtn.addEventListener("click", () => {
     // }
 
     // Opción 2
-    const dataContent = isShuffle ? "🟩 " : "⬛ ";
-    shuffleBtn.setAttribute('data-content', dataContent);
+    shuffleBtn.setAttribute('data-content', isShuffle ? "🟩 " : "⬛ ");
 });
 
 loopBtn.addEventListener("click", () => {
@@ -98,72 +97,53 @@ loopBtn.addEventListener("click", () => {
 
 // Volumen control
 volumeControl.addEventListener("input", (event) => {
-    volumeDisplay.innerHTML = volumeControl.value;
-    // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/volume
+    volumeDisplay.textContent = volumeControl.value;
     audio.volume = volumeControl.value / 100;
     if (volumeControl.value === "0") {
-        audioVolume = true;
-        volumeBtn.innerHTML = "🔈";
-        currentVolume = .5;
+        volumeBtn.textContent = "🔈";
+        audio.muted = true;
+        currentVolume = .5; // 50 %
     } else {
-        audioVolume = false;
         volumeBtn.textContent = "🔊";
-        currentVolume = volumeControl.value / 100;
+        audio.muted = false;
+        currentVolume = volumeControl.value / 100; //  Values between 0 y 100
     }
+
 });
 
-volumeBtn.addEventListener("click", (event) => {
-    audioVolume = !audioVolume;
-    if (audioVolume) {
+volumeBtn.addEventListener("click", () => {
+    audio.muted = !audio.muted;
+    if (audio.muted) {
         audio.volume = 0;
         volumeControl.value = 0;
-        volumeDisplay.textContent = 0;
-        volumeBtn.innerHTML = "🔈";
+        volumeBtn.textContent = "🔈";
     } else {
-        audio.volume = currentVolume; // 0.1 a 1
-        volumeControl.value = currentVolume * 100; // 1 a 100
+        audio.volume = currentVolume; // Values 0.1 and 1, where 1 = 100 %
+        volumeControl.value = currentVolume * 100; // Values are between 0 and 100
         volumeBtn.textContent = "🔊";
-        volumeDisplay.textContent = Math.floor(currentVolume * 100); // 1 a 100
     }
+    volumeDisplay.textContent = volumeControl.value;
 });
 // End volumen control
 
 // Mostrar la duración del audio cuando se haya cargado
 audio.addEventListener("loadedmetadata", () => {
-    const duration = formatTime(audio.duration); // Formatear la duración
-    durationDisplay.textContent = duration; // Mostrar la duración
+    durationDisplay.textContent = formatTime(audio.duration); // Formatear la duración y mostrar
     progressBar.max = audio.duration; // Ajustar el máximo de la barra de progreso
 });
 
 // Función para actualizar la barra de progreso
 audio.addEventListener("timeupdate", () => {
-    const currentTime = formatTime(audio.currentTime); // Formatear el tiempo actual
-    currentTimeDisplay.textContent = currentTime; // Mostrar el tiempo actual
+    currentTimeDisplay.textContent = formatTime(audio.currentTime); // Formatear el tiempo actual y mostrar
     progressBar.value = audio.currentTime;// Actualizar la barra de progreso
 });
 
 // Progress bar
 // Ajustar el tiempo de reproducción al mover la barra de progreso
 progressBar.addEventListener("input", () => {
-    // audio.pause();
     audio.currentTime = progressBar.value;  // Actualizar el tiempo del audio
 });
 
-// Antes de mover el control, guardamos el estado de reproducción del audio
-progressBar.addEventListener("mousedown", () => {
-    // wasPlaying = !audio.paused;  // Guarda si el audio está reproduciéndose
-    // if (wasPlaying) {  // Solo hace play si el audio estaba en reproducción
-    //     audio.play();  // Reproduce el audio después de que se haya dejado de mover
-    // }
-});
-
-// Reanudar el audio cuando el control deje de moverse
-progressBar.addEventListener("change", () => {
-    // if (wasPlaying) {  // Solo hace play si el audio estaba en reproducción
-    //     audio.play();  // Reproduce el audio después de que se haya dejado de mover
-    // }
-    // console.log("se movio la barra dentro del change!");
-});
 // End progress bar
 
 let currentSongIndex = 0;
@@ -330,7 +310,7 @@ audio.addEventListener('ended', function () {
 
 // Functions
 // Format time in hh:mm:ss or mm:ss
-function formatTime(seconds) {
+const formatTime = (seconds) => {
     /**
      * 1 h. = 60 m.
      * 1 m. = 60 s.
@@ -341,12 +321,12 @@ function formatTime(seconds) {
      **/
     const hours = (seconds / 3600) | 0;  // Hours calculation
     const minutes = ((seconds % 3600) / 60) | 0;  // Minutes calculation
-    const remainingSeconds = Math.floor(seconds % 60);  // Whole seconds
+    const remainingSeconds = Math.round(seconds % 60);  // Whole seconds
 
     // Conditional formatting: Only show hours if greater than 0
     return hours > 0
         // Show hours:minutes:seconds format
-        ? `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`
+        ? `${hours}:${/*minutes < 10 ? '0' + minutes : */minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`
         // Show minutes:seconds format
         : `${/*minutes < 10 ? '0' + minutes :*/ minutes}:${remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds}`;
 }
