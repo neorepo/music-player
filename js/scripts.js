5359c57ce2e4b6bea49242ab2b2e7f51246b6f3b
neorepo <neorepo.edu@gmail.com>
2025-09-05 00:05:52 -0300

    Updating files

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,19 @@
 const songs = [
     { id: 1, artist: "Eddie Santiago", title: "Antidoto y veneno", src: "audio/salsa/Eddie Santiago - Antidoto y veneno.m4a", duration: "5:18" },
     { id: 2, artist: "La inmensidad", title: "Ay que amor", src: "audio/salsa/La inmensidad - Ay que amor.mp3", duration: "3:54" },
-    { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: "5:43" },
-    { id: 4, artist: "Los Hermanos Moreno", title: "Por alguien como tu", src: "audio/salsa/Los Hermanos Moreno - Por alguien como tu.m4a", duration: "3:38" },
+    { id: 3, artist: "La Misma Gente", title: "Llego el final", src: "audio/salsa/La Misma Gente - Llego el final.m4a", duration: "5:44" },
+    { id: 4, artist: "Los Hermanos Moreno", title: "Por alguien como tu", src: "audio/salsa/Los Hermanos Moreno - Por alguien como tu.m4a", duration: "3:35" },
     { id: 5, artist: "Nino Segarra", title: "Como amigos si, como amantes no", src: "audio/salsa/Nino Segarra - Como amigos si, como amantes no.mp3", duration: "4:46" },
     { id: 6, artist: "Nino Segarra", title: "Entre la espada y la pared", src: "audio/salsa/Nino Segarra - Entre la espada y la pared.mp3", duration: "5:44" },
-    { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: "5:06" },
-    { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: "5:32" },
+    { id: 7, artist: "Nino Segarra", title: "Porque te amo", src: "audio/salsa/Nino Segarra - Porque te amo.m4a", duration: "5:07" },
+    { id: 8, artist: "Paquito Guzman", title: "Que voy a hacer sin ti", src: "audio/salsa/Paquito Guzman - Que voy a hacer sin ti.m4a", duration: "5:33" },
     { id: 9, artist: "Pedro Arroyo", title: "Todo me huele a ti", src: "audio/salsa/Pedro Arroyo - Todo me huele a ti.mp3", duration: "5:22" },
     { id: 10, artist: "The New York Band", title: "Nadie como tu", src: "audio/salsa/The New York Band - Nadie como tu.m4a", duration: "4:47" },
     { id: 11, artist: "Willie Gonzalez", title: "Hazme olvidarla", src: "audio/salsa/Willie Gonzalez - Hazme olvidarla.mp3", duration: "5:36" },
-    { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: "5:32" },
-    { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: "5:07" },
-    { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: "5:03" },
-    { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: "5:08" },
+    { id: 12, artist: "Johnny Rojas", title: "Adicto a ti", src: "audio/salsa/Johnny Rojas - Adicto a ti.m4a", duration: "5:26" },
+    { id: 13, artist: "Lalo Rodriguez", title: "Ven devorame otra vez", src: "audio/salsa/Lalo Rodriguez - Ven devorame otra vez.m4a", duration: "5:08" },
+    { id: 14, artist: "Frankie Ruiz", title: "Tu con el", src: "audio/salsa/Frankie Ruiz - Tu con el.m4a", duration: "5:04" },
+    { id: 15, artist: "David Pabon", title: "Aquel viejo motel", src: "audio/salsa/David Pabon - Aquel viejo motel.m4a", duration: "5:09" },
 ];
 
 // Ordenamos el listado de canciones por artista
@@ -53,7 +53,6 @@ const playPauseBtn = d.querySelector("#play-pause-btn");
 
 // Loop btn
 const loopBtn = d.querySelector("#loop-btn");
-let isLoop = false;
 
 // Previous btn
 const previousBtn = d.querySelector("#previous-btn");
@@ -69,9 +68,10 @@ shuffleBtn.addEventListener("click", () => {
     isShuffle = !isShuffle;
 
     // if (isShuffle) {
-    //     alert("Shuffle mode on!");
+    //     shuffle(songs);
+    //     console.log(songs);
     // } else {
-    //     alert("Shuffle mode off!");
+
     // }
 
     // Opción 1
@@ -86,9 +86,8 @@ shuffleBtn.addEventListener("click", () => {
 });
 
 loopBtn.addEventListener("click", () => {
-    isLoop = !isLoop;
-    audio.loop = isLoop;
-    loopBtn.textContent = isLoop ? "🔂" : "🔁";
+    audio.loop = !audio.loop;
+    loopBtn.textContent = audio.loop ? "🔂" : "🔁";
 });
 
 // Volumen control
@@ -307,6 +306,7 @@ audio.addEventListener('ended', () => {
 // Functions
 // Format time in hh:mm:ss or mm:ss
 const formatTime = (seconds) => {
+    seconds = Math.round(seconds);
     /**
      * 1 h. = 60 m.
      * 1 m. = 60 s.
@@ -317,7 +317,7 @@ const formatTime = (seconds) => {
      **/
     const hours = (seconds / 3600) | 0;  // Hours calculation
     const minutes = ((seconds % 3600) / 60) | 0;  // Minutes calculation
-    const remainingSeconds = Math.floor(seconds % 60);  // Whole seconds
+    const remainingSeconds = (seconds % 60);  // Whole seconds
 
     // Conditional formatting: Only show hours if greater than 0
     return hours > 0
